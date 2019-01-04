d96a2e796962d8466b5419cb976201f621b7bbe7
Paul <Sivivatu@users.noreply.github.com>
2019-01-04 21:26:35 +0000

    initial commit
    Created folder structure from Cookiecutter DataScience

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A synchronization and tracking process to analysis fitness data from multiple sources',
+    author='Paul Houghton',
+    license='',
+)
